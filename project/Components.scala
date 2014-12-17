93630eba864d9f7b19dbf1ff602b73735a74ccc8
Andy Ai <yanbo.ai@gmail.com>
2014-12-17 17:42:44 +0800

    Add dependencies

--- project/Components.scala
+++ project/Components.scala
@@ -1,13 +1,16 @@
 import sbt._
 
-
 object Version {
   val akka = "2.3.7"
+  val akkaHttp = "1.0-M1"
   val scalaTest = "2.2.2"
 }
 
 object Components {
   val dependencies = List(
-    "org.scalatest" %% "scalatest" % Version.scalaTest % "test"
+    "com.typesafe.akka" %% "akka-actor" % Version.akka withSources(),
+    "com.typesafe.akka" %% "akka-stream-experimental" % Version.akkaHttp withSources(),
+    "com.typesafe.akka" %% "akka-http-core-experimental" % Version.akkaHttp withSources(),
+    "org.scalatest" %% "scalatest" % Version.scalaTest % "test" withSources()
   )
 }
