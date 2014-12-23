784d9dbb12cba7331027222c35e7a6970a000ce5
Andy Ai <yanbo.ai@gmail.com>
2014-12-24 01:30:30 +0800

    Optimize dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,17 +4,22 @@ import sbt._
 
 object Dependencies {
 
+  val libraries = Seq(Compile.akka, Compile.akkaStream, Compile.akkaHttp, Test.scalaTest)
+
   object Versions {
     val akka = "2.3.7"
     val akkaHttp = "1.0-M1"
     val scalaTest = "2.2.2"
   }
 
-  val libraries = List(
-    "com.typesafe.akka" %% "akka-actor" % Versions.akka withSources(),
-    "com.typesafe.akka" %% "akka-stream-experimental" % Versions.akkaHttp withSources(),
-    "com.typesafe.akka" %% "akka-http-core-experimental" % Versions.akkaHttp withSources(),
-    "org.scalatest" %% "scalatest" % Versions.scalaTest % "test" withSources()
-  )
+  object Test {
+    val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test" withSources()
+  }
+
+  object Compile {
+    val akka = "com.typesafe.akka" %% "akka-actor" % Versions.akka withSources()
+    val akkaStream = "com.typesafe.akka" %% "akka-stream-experimental" % Versions.akkaHttp withSources()
+    val akkaHttp = "com.typesafe.akka" %% "akka-http-core-experimental" % Versions.akkaHttp withSources()
+  }
 
 }
