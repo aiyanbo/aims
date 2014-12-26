d96c57f5faad7f4c577c7da1baea92708b74c0ed
Andy Ai <yanbo.ai@gmail.com>
2014-12-26 19:53:05 +0800

    Main

--- aims-samples/src/main/scala/org/jmotor/samples/Main.scala
+++ aims-samples/src/main/scala/org/jmotor/samples/Main.scala
@@ -4,15 +4,15 @@ import org.jmotor.aims.Aims
 import org.jmotor.samples.services.{CouponOperationResource, UUIDService}
 
 /**
-  * Component:
-  * Description:
-  * Date: 2014/12/26
-  * @author Andy Ai
-  */
-class Main extends App {
-   val aims = Aims("hello-aims")
-   aims.registerService(new UUIDService)
-   aims.registerService(new CouponOperationResource)
-   aims.startup()
-   aims.awaitShutdown
- }
+ * Component:
+ * Description:
+ * Date: 2014/12/26
+ * @author Andy Ai
+ */
+object Main extends App {
+  val aims = Aims("hello-aims")
+  aims.registerService(new UUIDService)
+  aims.registerService(new CouponOperationResource)
+  aims.startup()
+  aims.awaitShutdown
+}
