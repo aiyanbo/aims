9edfdc6c70b1059c14ad9154b2457c8005ee5500
Andy Ai <yanbo.ai@gmail.com>
2015-01-22 00:26:22 +0800

    Let be route

--- aims-core/src/test/scala/akka/TestServer.scala
+++ aims-core/src/test/scala/akka/TestServer.scala
@@ -5,6 +5,7 @@ import akka.http.Http
 import akka.http.model.HttpResponse
 import akka.stream.FlowMaterializer
 import akka.http.server.Directives._
+import com.google.common.net.HttpHeaders
 
 object TestServer extends App {
   implicit val system = ActorSystem("my-test")
@@ -18,54 +19,26 @@ object TestServer extends App {
 
   val binding = Http().bind(interface = "localhost", port = 8080)
 
-  val x = {
+  val route = {
     get {
-      path("") {
-        complete(HttpResponse(entity = "you are index"))
-      } ~
-        path("secure") {
-          complete("Hello . Access has been granted!")
-
-        } ~
-        path("ping") {
-          complete("PONG!")
-        } ~
-        path("crash") {
-          complete(sys.error("BOOM!"))
-        }
+      extractRequestContext {
+        ctx =>
+          complete(ctx.request.uri.path.toString)
+      }
+    } ~ put {
+      optionalHeaderValueByName(HttpHeaders.CONTENT_TYPE) {
+        case Some(header) =>
+          extractRequestContext {
+            ctx => complete(ctx.request.uri.path.toString)
+          }
+        case None => complete("")
+      }
     }
   }
 
 
-
-  val materializedMap = binding startHandlingWith x
-
+  val materializedMap = binding startHandlingWith route
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  Console.readLine()
+  scala.io.StdIn.readLine()
   binding.unbind(materializedMap).onComplete(_ ⇒ system.shutdown())
-
-  lazy val index =
-    <html>
-      <body>
-        <h1>Say hello to
-          <i>akka-http-core</i>
-          !</h1>
-        <p>Defined resources:</p>
-        <ul>
-          <li>
-            <a href="/ping">/ping</a>
-          </li>
-          <li>
-            <a href="/secure">/secure</a>
-            Use any username and '
-            &lt;
-            username
-            &gt;
-            -password' as credentials</li>
-          <li>
-            <a href="/crash">/crash</a>
-          </li>
-        </ul>
-      </body>
-    </html>
 }
