116f99601147ccc9efbcdc8bfe5298037a6ddd90
Andy Ai <yanbo.ai@gmail.com>
2015-01-26 01:35:43 +0800

    Stream experimental

--- aims-core/src/test/scala/akka/TestServer.scala
+++ aims-core/src/test/scala/akka/TestServer.scala
@@ -17,30 +17,28 @@ object TestServer extends App {
 
   //  import akka.http.marshallers.xml.ScalaXmlSupport._
 
-
   val binding = Http().bind(interface = "localhost", port = 8080)
 
   val route = {
     get {
       extractRequestContext {
-        ctx =>
-          complete(ctx.request.uri.path.toString)
+        ctx ⇒
+          complete(ctx.request.uri.path.toString())
       }
     } ~ put {
       optionalHeaderValueByName(HttpHeaders.CONTENT_TYPE) {
-        case Some(header) =>
+        case Some(header) ⇒
           extractRequestContext {
-            ctx => complete(ctx.request.uri.path.toString)
+            ctx ⇒ complete(ctx.request.uri.path.toString())
           }
-        case None => complete("")
+        case None ⇒ complete("")
       }
     } ~ post {
       RouteResult.Complete(HttpResponse(entity = "you have post")) match {
-        case RouteResult.Complete(response) => complete(response)
+        case RouteResult.Complete(response) ⇒ complete(response)
       }
     }
   }
-
 
   val materializedMap = binding startHandlingWith route
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
