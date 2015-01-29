3004db5dc6f0c2d634c63da1ec954e126e2b27ae
Andy Ai <yanbo.ai@gmail.com>
2015-01-29 20:35:29 +0800

    Switch to case class

--- aims-core/src/main/scala/aims/routing/PatternMatcher.scala
+++ aims-core/src/main/scala/aims/routing/PatternMatcher.scala
@@ -19,40 +19,41 @@ trait PatternMatcher {
   def apply(path: Uri.Path): Option[Any]
 }
 
-class AkkaPathMatcher(pm: PathMatcher[_]) extends PatternMatcher {
-  override def apply(path: Path): Option[Any] = {
-    pm.apply(path) match {
-      case Matched(_, extractions) ⇒ Some(extractions)
-      case Unmatched               ⇒ None
-    }
-  }
-}
-
-class AimsPathMatcher(pm: String) extends PatternMatcher {
-  override def apply(path: Path): Option[Any] = {
-    val tokens = pm.split("/")
-    val parameters = ListBuffer[Int]()
-    for (i ← 0 to (tokens.length - 1)) {
-      val token = tokens(i)
-      if (token.matches("""(:|#)\w+""")) {
-        parameters += i
-      }
-    }
-    val matcher = pm.replaceAll("""#\w+""", "\\\\d+").replaceAll(""":\w+""", "(\\\\w+-?)+")
-    if (path.toString().matches(matcher)) {
-      if (parameters.isEmpty) {
-        Some(())
-      } else {
-        val segments = path.toString().split("/")
-        Some(Lists.toTuple(parameters.map(segments(_)).toList))
-      }
-    } else {
-      None
-    }
-  }
-}
-
 object PatternMatcher {
+
+  final case class AkkaPathMatcher(pm: PathMatcher[_]) extends PatternMatcher {
+    override def apply(path: Path): Option[Any] = {
+      pm.apply(path) match {
+        case Matched(_, extractions) ⇒ Some(extractions)
+        case Unmatched               ⇒ None
+      }
+    }
+  }
+
+  final case class AimsPathMatcher(pm: String) extends PatternMatcher {
+    override def apply(path: Path): Option[Any] = {
+      val tokens = pm.split("/")
+      val parameters = ListBuffer[Int]()
+      for (i ← 0 to (tokens.length - 1)) {
+        val token = tokens(i)
+        if (token.matches("""(:|#)\w+""")) {
+          parameters += i
+        }
+      }
+      val matcher = pm.replaceAll("""#\w+""", "\\\\d+").replaceAll(""":\w+""", "(\\\\w+-?)+")
+      if (path.toString().matches(matcher)) {
+        if (parameters.isEmpty) {
+          Some(())
+        } else {
+          val segments = path.toString().split("/")
+          Some(Lists.toTuple(parameters.map(segments(_)).toList))
+        }
+      } else {
+        None
+      }
+    }
+  }
+
   def apply(pm: PathMatcher[_]): PatternMatcher = {
     new AkkaPathMatcher(pm)
   }
