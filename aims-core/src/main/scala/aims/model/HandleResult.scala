1d8c140942153ae04482b371260e9cd233fafcf0
Andy Ai <yanbo.ai@gmail.com>
2015-01-29 19:23:54 +0800

    Implicit tools

--- aims-core/src/main/scala/aims/model/HandleResult.scala
+++ aims-core/src/main/scala/aims/model/HandleResult.scala
@@ -4,6 +4,7 @@ import akka.http.model.HttpResponse
 import akka.http.server.Rejection
 
 import scala.collection.immutable
+import scala.language.implicitConversions
 
 /**
  * Component:
@@ -19,6 +20,24 @@ object HandleResult {
 
   final case class Complete(response: HttpResponse) extends HandleResult
 
+  final case class Failure(causes: immutable.Seq[Throwable]) extends HandleResult
+
   final case class Rejected(rejections: immutable.Seq[Rejection]) extends HandleResult
+
+  implicit def success(result: Any): Success = {
+    Success(result)
+  }
+
+  implicit def complete(response: HttpResponse): Complete = {
+    Complete(response)
+  }
+
+  implicit def failure(causes: immutable.Seq[Throwable]): Failure = {
+    Failure(causes)
+  }
+
+  implicit def rejected(rejections: immutable.Seq[Rejection]): Rejected = {
+    Rejected(rejections)
+  }
 
 }
--- aims-core/src/test/scala/aims/res/CheckNameRes.scala
+++ aims-core/src/test/scala/aims/res/CheckNameRes.scala
@@ -1,10 +1,11 @@
 package aims.res
 
 import aims.core.RestRes
-import aims.model.{ HandleResult, Event }
+import aims.model.Event
 import aims.routing.PatternMatcher
 import aims.routing.Patterns._
 import akka.http.model.{ StatusCodes, HttpResponse, HttpMethods, HttpMethod }
+import aims.model.HandleResult._
 
 /**
  * Component:
@@ -20,9 +21,9 @@ class CheckNameRes extends RestRes {
   override def handle: Handle = {
     case event: Event ⇒
       event.payload match {
-        case Some(name) if name.length < 5 ⇒ HandleResult.Complete(HttpResponse(StatusCodes.BadRequest, entity = "Name's length must be then 5"))
-        case Some(name)                    ⇒ HandleResult.Complete(HttpResponse(StatusCodes.OK, entity = s"$name is available"))
-        case None                          ⇒ HandleResult.Complete(HttpResponse(StatusCodes.BadRequest, entity = "Name cannot be empty"))
+        case Some(name) if name.length < 5 ⇒ HttpResponse(StatusCodes.BadRequest, entity = "Name's length must be then 5")
+        case Some(name)                    ⇒ HttpResponse(StatusCodes.OK, entity = s"$name is available")
+        case None                          ⇒ HttpResponse(StatusCodes.BadRequest, entity = "Name cannot be empty")
       }
   }
 }
