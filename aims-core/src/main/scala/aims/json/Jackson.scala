aa51e458dac52185a0fec8c64bb5661bb8eca7a1
Andy Ai <yanbo.ai@gmail.com>
2015-03-12 18:15:40 +0800

    Def date format

--- aims-core/src/main/scala/aims/json/Jackson.scala
+++ aims-core/src/main/scala/aims/json/Jackson.scala
@@ -1,7 +1,9 @@
 package aims.json
 
+import java.text.SimpleDateFormat
+
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.{ DeserializationFeature, ObjectMapper, PropertyNamingStrategy, SerializationFeature }
+import com.fasterxml.jackson.databind._
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 /**
@@ -14,8 +16,10 @@ object Jackson {
   lazy val mapper = new ObjectMapper()
 
   mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+  mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false)
+  mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
   mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
-  mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false)
+  mapper.setDateFormat(new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ssZ"))
   mapper.registerModule(DefaultScalaModule)
 }
